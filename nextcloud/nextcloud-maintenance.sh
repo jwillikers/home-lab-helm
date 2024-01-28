90526f0966b17b04baead670fe1c00844baf600a
Jordan Williams <jordan@jwillikers.com>
2024-01-28 10:36:59 -0600

    Use a single script to execute common maintenance tasks

---
+++ nextcloud/nextcloud-maintenance.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# Run basic maintenance tasks for Nextcloud.
+
+/var/www/html/occ db:add-missing-columns
+/var/www/html/occ db:add-missing-indices
+/var/www/html/occ db:add-missing-primary-keys
+/var/www/html/occ app:update --all
