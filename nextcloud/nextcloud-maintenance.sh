a91af1855c8d9d84bd06bea0d26a7703f54bed05
Jordan Williams <jordan@jwillikers.com>
2024-09-29 10:54:21 -0500

    Run occ maintenance:repair --include-expensive

--- nextcloud/nextcloud-maintenance.sh
+++ nextcloud/nextcloud-maintenance.sh
@@ -2,7 +2,8 @@
 
 # Run basic maintenance tasks for Nextcloud.
 
+/var/www/html/occ app:update --all
 /var/www/html/occ db:add-missing-columns
 /var/www/html/occ db:add-missing-indices
 /var/www/html/occ db:add-missing-primary-keys
-/var/www/html/occ app:update --all
+/var/www/html/occ maintenance:repair --include-expensive
